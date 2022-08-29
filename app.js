b2a9410e5274cc4c0421051094a33d52fe2d9e53
luifer63 <lvix2000@gmail.com>
2022-08-29 17:57:12 -0500

    se actualiza para heroku

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
+app.listen( process.env.PORT|| 3000, () => console.log("Servidor corriendo en el puerto 3000"));
